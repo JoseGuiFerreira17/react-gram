5f04e4eb7c50d98bb669db7b0e6a3cfc4e02e78d
JoseGuiFerreira17 <joseguiferreira17@gmail.com>
2024-12-05 15:35:30 -0300

    initial backend settings

--- backend/app.jsx
+++ backend/app.jsx
@@ -1,13 +1,19 @@
+require("dotenv").config();
+
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
 
-const port = 6000;
+const port = process.env.PORT;
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+const router = require("./routes/Router.jsx");
+
+app.use(router);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
