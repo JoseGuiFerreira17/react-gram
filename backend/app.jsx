8805ec0810bea8fe92f3b58770740e2b9e9ac943
JoseGuiFerreira17 <joseguiferreira17@gmail.com>
2024-12-05 15:40:54 -0300

    initial middleware configurations

--- backend/app.jsx
+++ backend/app.jsx
@@ -5,11 +5,16 @@ const path = require("path");
 const cors = require("cors");
 
 const port = process.env.PORT;
+const url_origin = process.env.URL_ORIGIN;
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use(cors({ credentials: true, origin: url_origin }));
+
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 const router = require("./routes/Router.jsx");
 
