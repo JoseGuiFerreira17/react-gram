ddbba22e842f48c1024788f0cdcaec920b52cd15
JoseGuiFerreira17 <joseguiferreira17@gmail.com>
2024-12-07 10:51:51 -0300

    creation of the registration page

--- frontend/src/pages/Auth/Register.jsx
+++ frontend/src/pages/Auth/Register.jsx
@@ -1,7 +1,29 @@
 import "./Auth.css";
 
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+
 const Register = () => {
-  return <div>Register</div>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div>
+      <h2>ReactGram</h2>
+      <p className="subtitle">Cadastre-se para ver as fotos dos seu amigos</p>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Nome" />
+        <input type="email" placeholder="E-mail" />
+        <input type="password" placeholder="Senha" />
+        <input type="password" placeholder="Confirme sua senha" />
+        <input type="submit" value="Cadastrar" />
+      </form>
+      <p>
+        Já possui uma conta? <Link to="/login">Clique aqui.</Link>
+      </p>
+    </div>
+  );
 };
 
 export default Register;
